3ba184aa12948553998d9323fb7e591159e2cdd7
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-03 16:48:11 +0700

    config setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@pinia/nuxt"],
+
   // custom tailwindcss path
   tailwindcss:{
     cssPath: "~/assets/main.css"
   },
+
+  // server config variable
   runtimeConfig:{
     MONGO_URI: process.env.MONGO_URI
   },
+
+  // register nitro plugin
   nitro:{
     plugins:["@/server/db/index.ts"]
   },
+
+  /// transpile  afew packages
+  build: {
+    transpile: ["@headlessui/vue", "vue-toastification", "@headlessui/tailwindcss"],
+  },
+  
   compatibilityDate: "2025-02-03"
 })
