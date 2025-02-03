29ab69d67666688b8d1adc0b649783b2ca69853b
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-03 16:43:43 +0700

    สร้างไฟล์ tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", "@pinia/nuxt"],
+  // custom tailwindcss path
+  tailwindcss:{
+    cssPath: "~/assets/main.css"
+  },
   runtimeConfig:{
     MONGO_URI: process.env.MONGO_URI
   },
-
+  nitro:{
+    plugins:["@/server/db/index.ts"]
+  },
   compatibilityDate: "2025-02-03"
 })
